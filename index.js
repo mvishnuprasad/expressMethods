af60ea339cac00dac3bbc97c516631c46a70264f
Vishnu Prasad M <vpm735@gmail.com>
2021-12-13 17:36:17 +0530

    chaining

--- index.js
+++ index.js
@@ -17,9 +17,45 @@ app.get('/getItem', (req, res) =>
 //get the data and return 
 res.json(data)
 );
+app.get('/item/:id', (req, res, next) =>
+//get the id and return the corresponding json data
+// Convert the id to a number req.params.id
+//pass the id as json array index
+//next helps to create own middleware. only one response method is allowed in a single method
+//call
+{
+console.log(req.params.id);
+let user = Number(req.params.id);
+console.log(user);
+console.log(data[user])
+res.send(data[user])
+next();
+},
+(req,res)=>{
+    console.log('Second function')
+}
+);
+app.get('/rdr', (req, res) =>
+    res.redirect("https://www.flipkart.com/")
+);
+app.get('/img', (req, res) =>
+    res.download("img/bg.jpg")
+);
 app.put('/putItem', (req, res) =>
     res.send("Hello World! PUT")
 );
 app.delete('/deleteItem', (req, res) =>
     res.send("Hello World! Delete")
 );
+
+app.route('/Item')
+    .get((req, res) => {
+        res.send("New get response")
+    })
+    .put((req, res) =>
+    res.send("Hello World! PUT")
+    )
+    .delete( (req, res) =>
+    res.send("Hello World! Del")
+    );
+
